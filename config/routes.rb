7db89f6ad9c1673722aede2df73401faf4a3f41f
unknown <matad00rius@gmail.com>
2020-07-18 23:43:49 +0200

    remove player index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :establos, only: :show
-  resources :players, only: %i[index show]
+  resources :players, only: :show
 
   root to: 'establos/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
