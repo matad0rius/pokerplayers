90222549d64e22c1ef695900a8421dcbf689d9a2
unknown <matad00rius@gmail.com>
2020-07-18 23:55:34 +0200

    set views modify routes and going to sleep

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :establos, only: :show
   resources :players, only: :show
 
-  root to: 'establos/index'
+  root 'establos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
