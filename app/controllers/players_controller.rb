fe06c852d107ad790dfd1d60ca2e8b4b7a8539a4
unknown <matad00rius@gmail.com>
2020-07-19 17:14:12 +0200

    add search results to players and a search function

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,10 @@ class PlayersController < ApplicationController
   def show
     @player = Player.find(params[:id])
   end
+
+  def search_results
+    @query = params[:query]
+    @players = Player.where('name LIKE ?', "%#{@query}%")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :establos, only: :show
-  resources :players, only: :show
+
+  resources :players, only: :show do
+    collection do 
+      get 'search_results'
+    end
+  end
 
   root 'establos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
